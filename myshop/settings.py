fa00c3b2f5fcf6b5245fb51b16b480dc3c2d2a06
Federico Volpintesta <federicovolpintesta@MacBook-Pro-de-Federico.local>
2026-03-24 03:44:17 -0300

    Permitir dominios vercel y asterisco

--- myshop/settings.py
+++ myshop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-fs5527duaurkjkke()#)o5=y!*!6btt7&*vnq7qc6@=tzc!m(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '*']
 
 
 # Application definition
