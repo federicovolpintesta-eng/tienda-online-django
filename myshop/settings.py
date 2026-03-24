b1492b19865a01c729c2c513ed41f9c7519909b3
Federico Volpintesta <federicovolpintesta@MacBook-Pro-de-Federico.local>
2026-03-24 04:31:15 -0300

    Admin con estilo Jazzmin

--- myshop/settings.py
+++ myshop/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = ['*'] # Cambio final para el master
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
